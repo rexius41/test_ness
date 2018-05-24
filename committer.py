78fd0a3a0ebc1b403901d363a90024f5e0e34075
Tomas Kobezda <tomasko911@gmail.com>
2018-05-24 14:53:10 +0200

    made commiter to show only cimmiters names

--- committer.py
+++ committer.py
@@ -1,4 +1,6 @@
 def commit_er(size_array):
     s_array = len(size_array)
     for i in range(s_array):
-        print (size_array[i]['committer'])
+        x =(size_array[i]['commit'])
+        y=(x['committer'])
+        print(y['name'])
