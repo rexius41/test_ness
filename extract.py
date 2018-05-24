b9e4feeed356ff032792a834d8bc731ff6c23d24
Tomas Kobezda <tomasko911@gmail.com>
2018-05-24 14:13:25 +0200

    deletet token
    deleted token as github automatically delets it

--- extract.py
+++ extract.py
@@ -5,7 +5,7 @@ def rest_request(method, url, json=None):
         method,
         url,
         json=json,
-        headers= {"Authorization": "token " + "cdcd1328a5a90004ba6d5773c7d7a3cb2a0f0c60", "Accept": "application/vnd.github.v3+json"},
+        headers= {"Authorization": "token " + "", "Accept": "application/vnd.github.v3+json"},
         timeout=30)
     _status = _resp.headers["status"]
     if "200" in _status or "201" in _status:
