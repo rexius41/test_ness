ba8980519d4f025e9f72dda920f0267b24329429
rexius41 <tomasko911@gmail.com>
2018-05-27 21:35:46 +0200

    Adding cimmitters names to list

--- committer.py
+++ committer.py
@@ -1,6 +1,8 @@
+test = []
 def commit_er(size_array):
     s_array = len(size_array)
     for i in range(s_array):
         x =(size_array[i]['commit'])
         y=(x['committer'])
-        print(y['name'])
+        z = (y['name'])
+        test.insert(2, z)
